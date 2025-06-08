fdf9bd00e73b81c6e2cb4667ba53a76d65c8d2e1
Naufal F <naufalfirman098@gmail.com>
2025-06-09 05:14:55 +0700

    fix: correct left positioning of header text in Hero component for improved layout

--- src/app/(root)/_components/hero.tsx
+++ src/app/(root)/_components/hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 		<main id="beranda">
 			<header className="w-full relative lg:min-h-[80vh]">
 				<div className="container mx-auto px-6 md:pt-24 lg:px-10 lg:pt-32 py-16">
-					<h1 className="hidden lg:block font-bold capitalize text-[76px] absolute -left-40 top-80 -z-10 text-[#EEEEEE]/80 -rotate-90">
+					<h1 className="hidden lg:block font-bold capitalize text-[76px] absolute -left-[150px] top-80 -z-10 text-[#EEEEEE]/80 -rotate-90">
 						obin holiday
 					</h1>
 					<div className="w-full md:flex">
