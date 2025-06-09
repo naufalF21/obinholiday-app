7bfd4e30d6e1a3222b18d858fe7ac70b22c60730
Naufal F <naufalfirman098@gmail.com>
2025-06-10 06:02:32 +0700

    feat: implement International page with TravelCard component for displaying international destinations

--- src/components/ui/card/travel-card.tsx
+++ src/components/ui/card/travel-card.tsx
@@ -2,14 +2,24 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 import Image from "next/image";
 import React from "react";
 
-const TravelCard = () => {
+interface DestinationCardProps {
+	province: string;
+	location: string;
+	image: string;
+	rating: number;
+	price: number;
+}
+
+const TravelCard = ({ destination }: { destination: DestinationCardProps }) => {
+	const { province, location, image, rating, price } = destination;
+
 	return (
 		<div className="card-hover bg-white rounded-xl shadow-lg">
 			<div className="relative overflow-hidden rounded-t-xl h-56">
 				<Image
 					alt="Nusa Dua"
 					className="w-full h-full object-cover transform hover:scale-110 transition duration-500"
-					src="https://storage.googleapis.com/a1aa/image/0sh4Rqm1Wjsagm2ZcS4F9Rrm-HEyxFmddMZQQ2lkJwE.jpg"
+					src={image}
 					width={200}
 					height={200}
 				/>
@@ -23,7 +33,7 @@ const TravelCard = () => {
 							height="16"
 							className="text-primary"
 						/>
-						<span className="text-gray-600 text-sm">Bali</span>
+						<span className="text-gray-600 text-sm">{province}</span>
 					</div>
 					<div className="text-gray-600 text-sm flex items-center gap-2">
 						<Icon
@@ -32,11 +42,11 @@ const TravelCard = () => {
 							height="16"
 							className="text-yellow-400"
 						/>
-						<span>4.5</span>
+						<span>{rating}</span>
 					</div>
 				</div>
-				<h3 className="text-xl font-semibold">Nusa Dua Satu</h3>
-				<p className="text-gray-600 text-base">Rp. 500.000</p>
+				<h3 className="text-xl font-semibold">{location}</h3>
+				<p className="text-gray-600 text-base">Rp. {price.toLocaleString()}</p>
 				<button className="plus-button bg-primary text-white rounded-full p-2 hover:shadow-lg w-fit">
 					<Icon icon="mage:plus" className="w-6 h-6" />
 				</button>
